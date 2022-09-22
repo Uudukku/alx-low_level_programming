03d3862fd6f5d84d61c5da7efb8a74ef728ef797
Umar Muktar <umarmuktarsiddi@gmail.com>
2022-09-22 12:49:12 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code for ALX School students.
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
- * Return: Always 0.
-*/
-int main(void)
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
 {
-	char s1[] = "Hello";
-	char s2[] = "World!";
+	int i;
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
 	return (0);
 }
